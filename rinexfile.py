0deb6cbdd89cb72013a91374e925910de7cffb08
Félix Léger <leger@ipgp.fr>
2022-03-02 13:42:04 +0100

    Use Hatanaka library instead of CRZ2RNX and TEQC

--- rinexfile.py
+++ rinexfile.py
@@ -22,27 +22,43 @@ class RinexFile:
 
         self.path = rinexfile
         self.filename = self._filename()
-        self.rinex_data = self._load_rinex_data()
+        self.rinex_data, self.status = self._load_rinex_data()
 
 
     def _load_rinex_data(self):
         """
-        Load the uncompressed rinex data into a list var using hatanaka library
+        Load the uncompressed rinex data into a list var using hatanaka library.
+        Will return a table of lines of the uncompressed file, and a status.
+        Status 0 is OK. The other ones corresponds to the errors codes raised in
+        rinexarchive and in rinexmod.
+        01 - The specified file does not exists
+        02 - Not an observation Rinex file
+        03 - Invalid  or empty Zip file
+        04 - Invalid Compressed Rinex file
         """
 
         # Checking if existing file
         if not os.path.isfile(self.path):
-            # print('The provided Sitelog is not valid : ' + self.path)
-            return None
+            return None, 1
+
+        pattern = re.compile('....[0-9]{3}.\.[0-9]{2}(d\.((Z)|(gz)))')
+
+        if not pattern.match(os.path.basename(self.path)):
+            status = 2
+        else:
+            status = 0
 
         try:
             rinex_data = hatanaka.decompress(self.path).decode('utf-8')
             rinex_data = rinex_data.split('\n')
 
-        except:
-            return None
+        except ValueError:
+            return None, 3
 
-        return rinex_data
+        except hatanaka.hatanaka.HatanakaException:
+            return None, 4
+
+        return rinex_data, status
 
 
     def _filename(self):
@@ -79,7 +95,7 @@ class RinexFile:
         Returns a printable, with carriage-return, string of metadata lines from
         the header
         """
-        if self.rinex_data == None:
+        if self.status != 0:
             return ''
 
         metadata = []
@@ -115,6 +131,8 @@ class RinexFile:
         Available compressions are those of hatanaka compress function :
         'gz' (default), 'bz2', 'Z', or 'none'
         """
+        if self.status != 0:
+            return
 
         output_data = '\n'.join(self.rinex_data).encode('utf-8')
         output_data = hatanaka.compress(output_data, compression = compression)
@@ -127,6 +145,9 @@ class RinexFile:
 
 
     def get_station(self):
+
+        if self.status != 0:
+            return ''
 
         meta_station = 'MARKER NAME'
 
@@ -144,6 +165,9 @@ class RinexFile:
 
 
     def get_date(self):
+
+        if self.status != 0:
+            return ''
 
         meta_date = 'TIME OF FIRST OBS'
 
@@ -164,6 +188,9 @@ class RinexFile:
 
 
     def set_receiver(self, serial=None, type=None, firmware=None):
+
+        if self.status != 0:
+            return
 
         # Identify line that contains REC # / TYPE / VERS
         receiver_header_idx = next(i for i, e in enumerate(self.rinex_data) if 'REC # / TYPE / VERS' in e)
@@ -189,6 +216,9 @@ class RinexFile:
 
     def set_antenna(self, serial=None, type=None):
 
+        if self.status != 0:
+            return
+
         # Identify line that contains ANT # / TYPE
         antenna_header_idx = next(i for i, e in enumerate(self.rinex_data) if 'ANT # / TYPE' in e)
         antenna_meta = self.rinex_data[antenna_header_idx]
@@ -209,6 +239,9 @@ class RinexFile:
 
 
     def set_antenna_pos(self, X=None, Y=None, Z=None):
+
+        if self.status != 0:
+            return
 
         # Identify line that contains APPROX POSITION XYZ
         antenna_pos_header_idx = next(i for i, e in enumerate(self.rinex_data) if 'APPROX POSITION XYZ' in e)
@@ -240,6 +273,9 @@ class RinexFile:
 
     def set_antenna_delta(self, X=None, Y=None, Z=None):
 
+        if self.status != 0:
+            return
+
         # Identify line that contains ANTENNA: DELTA H/E/N
         antenna_delta_header_idx = next(i for i, e in enumerate(self.rinex_data) if 'ANTENNA: DELTA H/E/N' in e)
         antenna_delta_meta = self.rinex_data[antenna_delta_header_idx]
@@ -270,6 +306,9 @@ class RinexFile:
 
     def set_marker(self, station):
 
+        if self.status != 0:
+            return
+
         # Identify line that contains MARKER NAME
         marker_name_header_idx = next(i for i, e in enumerate(self.rinex_data) if 'MARKER NAME' in e)
         marker_name_meta = self.rinex_data[marker_name_header_idx]
@@ -289,6 +328,9 @@ class RinexFile:
 
 
     def set_agencies(self, operator=None, agency=None):
+
+        if self.status != 0:
+            return
 
         # Identify line that contains OBSERVER / AGENCY
         agencies_header_idx = next(i for i, e in enumerate(self.rinex_data) if 'OBSERVER / AGENCY' in e)
@@ -310,6 +352,9 @@ class RinexFile:
 
 
     def set_observable_type(self, observable_type):
+
+        if self.status != 0:
+            return
 
         # Identify line that contains RINEX VERSION / TYPE
         observable_type_header_idx = next(i for i, e in enumerate(self.rinex_data) if 'RINEX VERSION / TYPE' in e)
@@ -345,6 +390,9 @@ class RinexFile:
 
 
     def add_comment(self, comment):
+
+        if self.status != 0:
+            return
 
         end_of_header_idx = next(i for i, e in enumerate(self.rinex_data) if 'END OF HEADER' in e) + 1
         last_comment_idx = max(i for i, e in enumerate(self.rinex_data[0:end_of_header_idx]) if 'COMMENT' in e)
