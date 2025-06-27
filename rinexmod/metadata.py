8705939a1d4bd18124a44259d698fee1645d4c72
PiSa on TPX1 <psakicki@TPX1-GFZ>
2025-06-27 15:16:28 +0200

    add deprecation warnings for legacy imports in metadata, rinexfile, and rinexmod_api

--- rinexmod/metadata.py
+++ rinexmod/metadata.py
@@ -1,1 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 27/06/2025 15:10:30
+
+@author: psakic
+"""
+
+
+import warnings
+warnings.warn(
+    f"from rinexmod import metadata is deprecated, use import rinexmod.classes.metadata instead.",
+    category=DeprecationWarning,
+    stacklevel=2,
+)
+
+
 from .classes.metadata import *
--- rinexmod/rinexfile.py
+++ rinexmod/rinexfile.py
@@ -1,3 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 27/06/2025 15:10:30
+
+@author: psakic
+"""
+
+import warnings
+warnings.warn(
+    f"from rinexmod import rinexfile is deprecated, use import rinexmod.classes.rinexfile instead.",
+    category=DeprecationWarning,
+    stacklevel=2,
+)
 
 
 from .classes.rinexfile import *
