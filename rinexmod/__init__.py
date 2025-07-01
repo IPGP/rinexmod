9b9fb9b587ba88d43b4b611a801c8f5b9528d899
PiSa on TPX1 <psakicki@TPX1-GFZ>
2025-07-01 12:21:03 +0200

    Bump version: 3.7.3 → 3.7.4

--- rinexmod/__init__.py
+++ rinexmod/__init__.py
@@ -5,5 +5,5 @@ from . import gamit_meta
 from . import rinexmod_api
 from . import get_m3g
 
-__version__='3.7.3' #  changed automaticcaly with bump-my-version
+__version__='3.7.4' #  changed automaticcaly with bump-my-version
 
